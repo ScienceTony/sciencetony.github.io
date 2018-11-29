1a39bdd706b645409b8f2e0a440322abd82dcaa0
Tony McDonald <dreamazium@gmail.com>
2018-11-29 10:08:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,12 @@ define('ISITSAFETORUN', TRUE);
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set('Etc/UTC');
-require 'head.php';
+
 $stylesheet = '';
 $javascript = '';
 $mycss      = '';
 $pagetitle  = 'Welcome to Tony McDonald\'s page';
+require 'head.php';
 ?>
 
 <?php
